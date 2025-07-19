dedad7f682ab5da65f107b3c40084ecbf3b83866
Jaisapthagiri <imjai6504@email.com>
2025-07-19 22:24:38 +0530

    added somemore

--- src/data/constants.jsx
+++ src/data/constants.jsx
@@ -20,7 +20,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Jaisapthagiri",
   resume:
-    "https://drive.google.com/file/d/1szBSvwUZ3c3W-6bN0Me3CXRND7MqKoDM/view?usp=drive_link",
+    "https://drive.google.com/file/d/10h9VeAYxaGmzw5zBnOxfqyfPFhZLIBVK/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/jaisapthagiri-j-869a25319/",
   insta: "https://www.instagram.com/i_jai06/",
   facebook:"https://www.facebook.com/profile.php?id=100035262488857"
